08310721f2080e9fc91f7efcad8552a0b0992950
snowfox-rubait <266122632+snowfox-rubait@users.noreply.github.com>
2026-04-19 02:32:47 +0600

    testing

--- common_shell.sh
+++ common_shell.sh
@@ -5,12 +5,12 @@ alias dot='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 gdot() {
     local msg="${1:-update configs}"
+    git -C ~/.dotfiles status --short  # <--- Add this to see a summary first
     git -C ~/.dotfiles add .
     git -C ~/.dotfiles commit -m "$msg"
     git -C ~/.dotfiles push
     echo "🚀 Dotfiles pushed to GitHub!"
 }
-
 # My SSH Keys
 if [ -z "$SSH_AUTH_SOCK" ]; then
     eval "$(ssh-agent -s)" > /dev/null
